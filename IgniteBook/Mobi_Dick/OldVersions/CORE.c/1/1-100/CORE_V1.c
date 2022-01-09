50a146c51ec6b60b18a53c0a53de2e50ced608f7
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-01-08 17:24:51 -0800

    CORE_V1.c
    An archive of the first version of the core file for this library.

---
+++ IgniteBook/Mobi_Dick/OldVersions/CORE.c/1/1-100/CORE_V1.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <string.h>
+#include <headers.h>
+// Start of script
+/* 
+Mobi Dick
+A C-based library for emulating MobiPockets Mobi book file format
+*/
+// Note: I am inexperienced with the C language, so this is not functional yet, and serves as pseudocode
+// Mobi file format method
+int mobiFormat {
+  // Mobi file format
+  mobi = string("mobi\n" + "MOBI\n"); // Defining the file format
+  break;
+}
+// Reading mobi files from the year 2000 method
+int mobi2000 {
+  // Legacy Y2000 MobiPocket support
+  // This function is coming soon
+  printf("Mobi2000 is currently unsupported\n");
+  break;
+}
+// Reading mobi file method
+int mobiRead {
+  // Reading Mobi files
+  // This function is coming soon
+  printf("Unable to open MOBI file\n");
+  break;
+}
+// About page for the past MobiPocket software and company
+int aboutMobiPocket {
+  // About MobiPocket
+  printf("About MobiPocket\n");
+  printf("MobiPocket was a French company that produced software for eBook readers. It was founded in the year 2000, and was later bought by Amazon. It went defunct in 2016.\n");
+  sleep(5);
+  continue;
+  break;
+}
+// About page for the Mobi Dick software library
+int aboutMobiDickSoftware }
+  // About the Mobi Dick Library
+  printf("About Mobi Dick (library)\n");
+  printf("Mobi Dick is an open source software library written in C for use in the BlazeOS operating system project. It emulates the functionality of MobiPocket Mobi files. It is planned to work on other non-Linux operating systems in the future.\n");
+  printf("Mobi Dick is written by @seanpm2001.\n");
+  printf("🄯 Copyleft (2022-2022) Seanpm2001 and the BlazeOS software project.\n");
+  printf("This program is free software: you can redistribute it and/or modify\n");
+  printf("it under the terms of the GNU General Public License as published by\n");
+  printf("the Free Software Foundation, either version 3 of the License, or\n");
+  printf("(at your option) any later version.\n\n");
+  printf("This program is distributed in the hope that it will be useful,\n");
+  printf("but WITHOUT ANY WARRANTY; without even the implied warranty of\n");
+  printf("MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n");
+  printf("GNU General Public License for more details.\n\n");
+  printf("You should have received a copy of the GNU General Public License\n");
+  printf("along with this program.  If not, see <https://www.gnu.org/licenses/>.\n");
+  break;
+}
+// Page turn animation support method
+int pageTurn {
+  // Page turn animations
+  // This function is coming soon
+  printf("Page turn functionality is coming soon!\n");
+  break;
+}
+// WebMobi method for HTML and CSS support
+int webMobi {
+  // HTML and CSS support for Mobi
+  // This function is coming soon
+  printf("WebMobi support is coming soon!\n");
+  break;
+}
+// Main method (calls everything)
+int main {
+  return mobiFormat();
+  sleep(0.05);
+  return mobi2000();
+  sleep(0.05);
+  return mobiRead();
+  sleep(0.05);
+  return aboutMobiPocket();
+  sleep(0.05);
+  return aboutMobiDickSoftware();
+  sleep(0.05);
+  return pageturn();
+  sleep(0.05);
+  return webMobi();
+  sleep(10);
+  break;
+}
+// Start of program
+// Initialization
+return main();
+wait(300);
+continue;
+return 0;
+exit;
+/* File info
+* File version: 1 (2022, Saturday, January 8th at 5:24 pm)
+* File type: C Source file (*.c *.h)
+* Line count (including blank lines and compiler line): 104
+*/
+// This script is incomplete, and needs lots of work
+// End of script
