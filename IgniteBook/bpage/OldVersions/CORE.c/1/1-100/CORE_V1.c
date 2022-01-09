f81c0ec39f278f6ab8cef0c1dc4131aa41bc6281
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-01-08 18:17:48 -0800

    CORE_V1.c
    An archive of the first version of the core C file for this directory.

---
+++ IgniteBook/bpage/OldVersions/CORE.c/1/1-100/CORE_V1.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <string.h>
+#include <headers.h>
+// Start of script
+/* 
+bpage
+A C-based library for supporting various animations for the IgniteBook subsystem and further book reading
+*/
+// Note: I am inexperienced with the C language, so this is not functional yet, and serves as pseudocode
+// Page animation method
+int bpage_Animation {
+	// Animations are not ready yet
+	return false;
+	break;
+}
+// About page for the Epub Dick software library
+int aboutbpageSoftware {
+  // About the Epub Dick Library
+  printf("About bpage (LIBrary)\n");
+  printf("bpage is a FLOSS, GPL-licensed software library to add various animations and features designed for the BlazeOS operating systems IgniteBook subsystem. It is written in the C programming language.\n");
+  printf("bpage is written by @seanpm2001.\n");
+  printf("🄯 Copyleft (2022-2022) Seanpm2001 and the BlazeOS software project.\n");
+  printf("This program is free software: you can redistribute it and/or modify\n");
+  printf("it under the terms of the GNU General Public License as published by\n");
+  printf("the Free Software Foundation, either version 3 of the License, or\n");
+  printf("(at your option) any later version.\n\n");
+  printf("This program is distributed in the hope that it will be useful,\n");
+  printf("but WITHOUT ANY WARRANTY; without even the implied warranty of\n");
+  printf("MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n");
+  printf("GNU General Public License for more details.\n\n");
+  printf("You should have received a copy of the GNU General Public License\n");
+  printf("along with this program.  If not, see <https://www.gnu.org/licenses/>.\n");
+  break;
+}
+// Page turn animation support method
+int pageTurn {
+  // Page turn animations
+  // This function is coming soon
+  printf("Page turn functionality is coming soon!\n");
+  break;
+}
+// Show the current page count (toggle-able option)
+int pageCount {
+	// For now, this is pseudocode
+	/* Notes
+	You know how the Kindle operating system doesn't tell you the page count, and instead tells you the location data of where you are?
+	This script will instead grab the page number */
+	scanf(%book.pdf && %book.epub);
+	break;
+}
+// Show the current location in the book
+int locationCounter {
+	// For now, this is pseudocode
+	/* Notes
+	You know how the Kindle operating system doesn't tell you the page count, and instead tells you the location data of where you are.
+	This script will do that */
+	scanf(%book.pdf && %book.epub);
+	break;
+}
+// Return the book completion percentage as a percentage on the book cover, and its metadata
+int bookCompletion {
+	// For now, this is pseudocode, very very bad pseudocode
+	newBook = double(1.0) for pageCount(sample(256)0);
+	for newBook.pageCount {
+		(newBook.pageCount / 100.00);
+		return pageCount as percentage;
+		break;
+	}
+	break;
+}
+// Main method (calls everything)
+int main {
+  return bpage_animation();
+  sleep(0.05);
+  return aboutbpageSoftware();
+  sleep(0.05);
+  return pageCount();
+  sleep(0.05)
+  return locationCounter();
+  sleep(0.05)
+  return bookCompletion();
+  sleep(0.05)
+  return pageturn();
+  sleep(10);
+  break;
+}
+// Start of program
+// Initialization
+return main();
+wait(300);
+continue;
+return 0;
+exit;
+/* File info
+* File version: 1 (2022, Saturday, January 8th at 6:17 pm)
+* File type: C Source file (*.c *.h)
+* Line count (including blank lines and compiler line): 101
+*/
+// This script is incomplete, and needs lots of work
+// End of script
