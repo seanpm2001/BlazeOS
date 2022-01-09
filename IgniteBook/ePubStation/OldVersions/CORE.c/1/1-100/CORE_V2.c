93ca36e35960bbc485519a952909df5e71a47037
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-01-08 18:06:37 -0800

    CORE_V2.c
    An archive of the 2nd version of the core C file for this project.

---
+++ IgniteBook/ePubStation/OldVersions/CORE.c/1/1-100/CORE_V2.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <string.h>
+#include <headers.h>
+// Start of script
+/* 
+ePubStation
+A C-based library for emulating ePub file format functionality as a book
+*/
+// Note: I am inexperienced with the C language, so this is not functional yet, and serves as pseudocode
+// Epub file format method
+int epubFormat {
+  // ePUB file format
+  epub = string("epub\n" + "EPUB\n"); // Defining the file format
+  break;
+}
+// Reading epub file method
+int epubRead {
+  // Reading Epub files
+  // This function is coming soon
+  printf("Unable to open EPUB file\n");
+  break;
+}
+// About page for the EpubStation software library
+int aboutEpubStationSoftware {
+  // About the EpubStation Library
+  printf("About EpubStation (LIBrary)\n");
+  printf("ePubStation is a FLOSS, GPL-licensed ePub reader designed for the BlazeOS operating systems IgniteBook subsystem. It is written in the C programming language.\n");
+  printf("EpubStation is written by @seanpm2001.\n");
+  printf("🄯 Copyleft (2022-2022) Seanpm2001 and the BlazeOS software project.\n");
+  printf("This program is free software: you can redistribute it and/or modify\n");
+  printf("it under the terms of the GNU General Public License as published by\n");
+  printf("the Free Software Foundation, either version 3 of the License, or\n");
+  printf("(at your option) any later version.\n\n");
+  printf("This program is distributed in the hope that it will be useful,\n");
+  printf("but WITHOUT ANY WARRANTY; without even the implied warranty of\n");
+  printf("MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n");
+  printf("GNU General Public License for more details.\n\n");
+  printf("You should have received a copy of the GNU General Public License\n");
+  printf("along with this program.  If not, see <https://www.gnu.org/licenses/>.\n");
+  break;
+}
+// Page turn animation support method
+int pageTurn {
+  // Page turn animations
+  // This function is coming soon
+  printf("Page turn functionality is coming soon!\n");
+  break;
+}
+// Main method (calls everything)
+int main {
+  return epubFormat();
+  sleep(0.05);
+  return epubRead();
+  sleep(0.05);
+  return aboutEpubStationSoftware();
+  sleep(0.05);
+  return pageturn();
+  sleep(10);
+  break;
+}
+// Start of program
+// Initialization
+return main();
+wait(300);
+continue;
+return 0;
+exit;
+/* File info
+* File version: 2 (2022, Saturday, January 8th at 6:06 pm)
+* File type: C Source file (*.c *.h)
+* Line count (including blank lines and compiler line): 75
+*/
+// This script is incomplete, and needs lots of work
+// End of script
