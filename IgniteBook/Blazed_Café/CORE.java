6013cd4f0db0ba51de2382cdfec70c023ae7d531
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-01-08 17:38:01 -0800

    CORE.java (V1)
    Version 1 of the core Java file for this library.

---
+++ IgniteBook/Blazed_Café/CORE.java
@@ -0,0 +1,27 @@
+// Start of script
+/* 
+Blazed café
+Java Libraries and support for BlazeOS's IgniteBook subsystem
+*/
+// Please note: this file is far from complete, and is not functional yet, as I am not too good with the Java programming language.
+import java.util.*; // Import all Java utilities
+public class blazedCafe {
+  public static void main(String[] args) {
+    public void aboutCoffeeHouse {
+     System.out.println("About the Blazed café (software library)\n");
+     System.out.println("Blazed café is a software library for BlazeOS's Ignitebook subsystem that enables Java functionality in the system.\n");
+     break;
+    }
+  return aboutCoffeeHouse()
+  break;
+  }
+  return main();
+  break;
+}
+// I don't know what to add right now
+/* File info
+* File type: Java source file (*.java)
+* File version: 1 (Saturday, 2022 January 8th at 5:38 pm)
+* Line count (including blank lines and compiler line): 28
+**/
+// End of script
